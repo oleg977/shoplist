9898541795923538f49de6713e730cd7d4e5071b
oleg <0284612g@gmail.com>
2025-11-04 20:39:22 +0300

    add code shoplist

--- shoplist/shoplist/urls.py
+++ shoplist/shoplist/urls.py
@@ -6,7 +6,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('products.urls')),  # Маршруты для товаров
-    path('users/', include('users.urls')),  # Маршруты для пользователей
+    path('users/', include('users.urls')),
+    # Маршруты для пользователей
 ]
 
 if settings.DEBUG:
--- shoplist/users/views.py
+++ shoplist/users/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.decorators import login_required
+from .forms import CustomUserCreationForm, CustomAuthenticationForm
 
-# Create your views here.
+# Регистрация
+def register_view(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')  # Перенаправляем на главную страницу
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'users/register.html', {'form': form})
+
+# Вход
+def login_view(request):
+    if request.method == 'POST':
+        form = CustomAuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('home')  # Перенаправляем на главную страницу
+    else:
+        form = CustomAuthenticationForm()
+    return render(request, 'users/login.html', {'form': form})
+
+# Выход
+@login_required
+def logout_view(request):
+    logout(request)
+    return redirect('login')  # Перенаправляем на страницу входа
