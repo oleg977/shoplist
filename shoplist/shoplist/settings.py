789c8e9a7d30e0f134197851ab9257d3b3d3adf0
oleg <0284612g@gmail.com>
2025-11-06 22:57:13 +0300

    add code shoplist

--- shoplist/shoplist/settings.py
+++ shoplist/shoplist/settings.py
@@ -24,13 +24,13 @@ SECRET_KEY = 'django-insecure-dtq=*-j!&y2)9t$^@#khylr9tb)q-(598(dfn#my_nk-^#6t+w
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True  # <--- Исправлено: поставь True для локальной разработки
+DEBUG = False # <--- Исправлено: поставь True для локальной разработки
 
 # Если DEBUG = False, Django требует указать ALLOWED_HOSTS.
 # Для локальной разработки можно указать:
 ALLOWED_HOSTS = ["127.0.0.1", "localhost"]  # <--- Добавь этот список
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['shoplist.onrender.com']
 # Application definition
 AUTH_USER_MODEL = 'users.User'
 INSTALLED_APPS = [
@@ -48,7 +48,9 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 # Настройки для статических файлов
+import os
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [BASE_DIR / 'static']
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -126,7 +128,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
